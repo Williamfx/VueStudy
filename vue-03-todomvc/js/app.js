758193f9df93df122e4861a5dc80793b3404b9b3
Williamfx <94459976@qq.com>
2021-02-01 17:33:11 +0800

    11111

--- vue-03-todomvc/js/app.js
+++ vue-03-todomvc/js/app.js
@@ -18,10 +18,28 @@
 		}
 	]
 
+	Vue.directive('app-focus', {
+		inserted(el, binding) {
+			el.focus()
+		}
+	})
+
 	var app = new Vue({
 		el: '#todoapp',
 		data: {
-			items
+			items,
+			currentItem: null,
+			filterStatus:'all'
+		},
+		directives: {
+			//定义时不要在前面加v-,引用指令时要加上v-
+			'todo-focus': {
+				update(el, binding) {//每当指令的值更新hou，会调用此函数
+					if (binding.value) {
+						el.focus()
+					}
+				}
+			}
 		},
 		computed: {
 			//复选框计算属性（双向绑定）
@@ -41,6 +59,25 @@
 			}
 		},
 		methods: {
+			//编辑完成
+			finishEdit(item, index, event) {
+				const content = event.target.value.trim();
+				//1.如果为空，则进行删除任务项
+				if (!event.target.value.trim()) {
+					this.removeItem(index)
+					return
+				}
+				//2.添加数据到任务项中
+				item.content = content
+				this.currentItem = null
+			},
+			// 进入编辑状态,当前点击的任务项item赋值currentItem，用于页面判断显示 .editing
+			toEdit(item) {
+				this.currentItem = item
+			},
+			cancelEdit() {
+				this.currentItem = null
+			},
 			//移除所有未完成项
 			removeCompleted() {
 				//过滤出所有未完成的任务，重新赋值数组即可
@@ -77,4 +114,15 @@
 		}
 	})
 
+	//当路由 hash 值改变后会自动调用此函数
+	window.onhashchange = function () {
+		console.log('hash改变了', window.location.hash)
+		//1.获取点击的路由 hash , 当截取的 hash 不为空返回截取的，为空时返回 'all'
+		const hash = window.location.hash.substr(2) || 'all'
+		console.log('hash',hash)
+		//2.状态一旦改变，将hash赋值给filterStatus
+		//  当计算属性filterItems感知到filterStatus变化后，就会重新过滤
+		//  当filterItems重新过滤出目标数据后，则自动同步更新到视图中
+		app.filterStatus = hash
+	}
 })(Vue);
